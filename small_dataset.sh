4344c89b9b75768aeb82515346b9a9493adb1c26
Sepehr Pourabolfath Hashtroudi <sepehr8@gra-login3.graham.sharcnet>
2022-03-11 08:05:21 -0500

    dataset final

---
+++ small_dataset.sh
@@ -0,0 +1,35 @@
+
+mkdir ./dataset/small
+a=500
+echo $a
+# head -$a ./dataset/evosuit/Evosuit_train.methods   > ./dataset/small/train_evo_80.methods
+head -$a ./dataset/train_final.methods   > ./dataset/small/train_final_500.methods
+# cat ./dataset/evosuit/Evosuit_train.methods >> ./dataset/small/train_combined.methods
+# cat ./dataset/small/train_combined.methods | wc -l
+# head -$a ./dataset/evosuit/Evosuit_train.tests   > ./dataset/small/train_evo_80.tests
+head -$a ./dataset/train_final.tests   > ./dataset/small/train_final_500.tests
+# cat ./dataset/evosuit/Evosuit_train.tests >> ./dataset/small/train_combined.tests
+# cat ./dataset/small/train_combined.tests | wc -l
+
+a=500
+echo $a
+# head -$a ./dataset/evosuit/Evosuit_test.methods   > ./dataset/small/eval_evo_80.methods
+head -$a ./dataset/eval_final.methods   > ./dataset/small/eval_final_500.methods
+# cat ./dataset/evosuit/Evosuit_test.methods >> ./dataset/small/eval_combined.methods
+# cat ./dataset/small/eval_combined.methods | wc -l
+# head -$a ./dataset/evosuit/Evosuit_test.tests   > ./dataset/small/eval_evo_80.tests
+head -$a ./dataset/eval_final.tests   > ./dataset/small/eval_final_500.tests
+# cat ./dataset/evosuit/Evosuit_test.tests >> ./dataset/small/eval_combined.tests
+# cat ./dataset/combined/eval_combined.tests | wc -l
+
+
+
+
+
+
+
+
+
+
+
+
